6a59367a3ef60b7df28f4ccf25d8f8bb6db883a8
KaduVieira13 <carloseduardo_alencar@outlook.com>
2021-04-12 08:11:41 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Maior2Numeros_DecisaoSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numeroA, numeroB;
+
+
+            Console.WriteLine("--- Maior 2 numeors ---");
+
+            Console.Write("Digite um numero..:");
+            numeroA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite um numero..:");
+            numeroB = Convert.ToInt32(Console.ReadLine());
+            if (numeroA > numeroB)
+            {
+                Console.WriteLine($" {numeroA} é maior que {numeroB}.");
+            }
+
+            if (numeroB > numeroA)
+            {
+                Console.WriteLine($"{numeroB} é maior que {numeroA}.");
+            }
+
+            if (numeroA == numeroB)
+            {
+                Console.WriteLine("Os numeros digitados são iguais.");
+            }
+
+
+        }
+    }
+}
